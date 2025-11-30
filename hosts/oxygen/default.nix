7f807e730b8c9af2d7e0a1a4defb3eed56407d02
Tom Fitzhenry <tom@tom-fitzhenry.me.uk>
2025-11-30 17:49:00 +1100

    oxygen: oops, fix by setting mandatory tomf.sshd.enable

--- hosts/oxygen/default.nix
+++ hosts/oxygen/default.nix
@@ -19,9 +19,12 @@
 
   boot.initrd.luks.devices."enc".device = "/dev/disk/by-uuid/b210a23c-b423-466d-afd1-c383a1b37a64";
 
-  tomf.rootfs = {
-    device = "/dev/mapper/enc";
-    subvolume = "/";
+  tomf = {
+    rootfs = {
+      device = "/dev/mapper/enc";
+      subvolume = "/";
+    };
+    sshd.enable = false;
   };
 
   boot.binfmt.emulatedSystems = [
