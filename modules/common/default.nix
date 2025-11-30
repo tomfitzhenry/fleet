18b23ddd31c3706f786d81941c136cb8f3b56ea9
Tom Fitzhenry <tom@tom-fitzhenry.me.uk>
2025-11-30 17:49:12 +1100

    common: add some knownHosts

--- modules/common/default.nix
+++ modules/common/default.nix
@@ -28,4 +28,24 @@
       }
     ];
   };
+
+  programs.ssh = {
+    knownHosts = {
+      "codeberg.org" = {
+        # https://codeberg.org/Codeberg/org/src/commit/c7f344a65ffefd959878b568ec4740946851797e/Imprint.md?display=source#L59
+        publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIVIC02vnjFyL+I4RHfvIGNtOgJMe769VTF1VR4EB3ZB";
+      };
+      "github.com" = {
+        # https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/githubs-ssh-key-fingerprints
+        publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
+      };
+      "gitlab.com" = {
+        # https://docs.gitlab.com/ee/user/gitlab_com/index.html#ssh-host-keys-fingerprints
+        publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAfuCHKVTjquxvt6CM6tdG4SLp1Btn/nOeHHE5UOzRdf";
+      };
+      "oncilla.mythic-beasts.com" = {
+        publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDIdfs0v4y4Qt1a2n7qOMtiI1fQPzqqYUqUFKSK0u7Fq";
+      };
+    };
+  };
 }
