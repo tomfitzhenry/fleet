076b893dc18356be4ec16ff15f5e832d508114c8
Tom Fitzhenry <tom@tom-fitzhenry.me.uk>
2025-11-30 19:07:46 +1100

    sshd: add other yubikey and document the serials

--- modules/sshd/default.nix
+++ modules/sshd/default.nix
@@ -3,8 +3,12 @@
 let
   cfg = config.tomf.sshd;
   keys = [
+    # Yubikey serial 13834265
     "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIA13oQrkygCx4G9HVeIjdItLtpZUmS2ICjMfmD0GPeGjAAAABHNzaDo= oxygen-sk"
+    # Yubikey serial 10574875
     "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAICpd1nSXGyN375CCLYmHH3VvhsqRTTJO3eS2vdBL/702AAAABHNzaDo= portable-yubikey"
+    # Yubikey serial 11120573
+    "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIN3Je4I0D9gf6krw+HhM5X0Fdg1sq5bf3VDvLBDxZ3XAAAAABHNzaDo= ozdesk-sk"
   ];
 in
 {
