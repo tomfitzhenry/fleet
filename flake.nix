1322c085be4f5269edbb915ca0eec8e11553dd04
Tom Fitzhenry <tom@tom-fitzhenry.me.uk>
2025-11-30 15:55:20 +1100

    add comin

--- flake.nix
+++ flake.nix
@@ -1,13 +1,20 @@
 {
   inputs = {
     nixpkgs-stable.url = "github:nixos/nixpkgs?ref=nixos-25.11";
+    comin = {
+      url = "github:nlewo/comin";
+      inputs.nixpkgs.follows = "nixpkgs-stable";
+    };
   };
-  outputs = { nixpkgs-stable, ... }:
+  outputs = { comin, nixpkgs-stable, ... }:
     let
       mkStableMachine = hostname: nixpkgs-stable.lib.nixosSystem {
         modules = [
 	  ./hosts/${hostname}
 	  { networking.hostName = hostname; }
+
+	  comin.nixosModules.comin
+
           ./modules/common
         ];
       };
--- modules/common/default.nix
+++ modules/common/default.nix
@@ -5,4 +5,14 @@
   ];
 
   services.tailscale.enable = true;
+
+  services.comin = {
+    enable = true;
+    remotes = [{
+      name = "origin";
+      url = "https://codeberg.org/tomf/fleet";
+      branches.main.name = "master";
+      poller.period = 60*5; # 5 mins
+    }];
+  };
 }
