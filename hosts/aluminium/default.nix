1d5a473131bcb94378c848a9ce54f5ae35c15762
Tom Fitzhenry <tom@tom-fitzhenry.me.uk>
2025-12-07 17:08:04 +1100

    aluminium: shut down vm-alma
    Not needed.

--- hosts/aluminium/default.nix
+++ hosts/aluminium/default.nix
@@ -1,4 +1,4 @@
-# Optiplex 7070 Micro, a VM host.
+# Optiplex 7070 Micro, a VM/container host.
 {
   config,
   pkgs,
@@ -56,21 +56,5 @@
     device = "rockpro64:/export/share";
     fsType = "nfs";
     neededForBoot = false;
-  };
-
-  # Connect with "screen /dev/pts/1"
-  systemd.services.vm-alma = {
-    wantedBy = [ "multi-user.target" ];
-    after = [ "network.target" ];
-    serviceConfig = {
-      ExecStartPre = [
-        "${pkgs.bash}/bin/bash -c '${pkgs.iproute2}/bin/ip link del vm-alma || true'"
-        "${pkgs.iproute2}/bin/ip link add link eno2 name vm-alma type macvtap"
-        "${pkgs.iproute2}/bin/ip link set vm-alma address 6a:c8:f1:8d:78:c1 up"
-        "${pkgs.iproute2}/bin/ip link set vm-alma up"
-      ];
-      ExecStart = "${lib.getExe pkgs.bash} -c '${lib.getExe pkgs.cloud-hypervisor} --disk path=/srv/vm/alma.raw --memory size=4G --firmware /srv/vm/hypervisor-fw --serial pty --console off --net fd=3,mac=$(cat /sys/class/net/vm-alma/address) 3<>/dev/tap$(cat /sys/class/net/vm-alma/ifindex)' --cmdline console=hvc0";
-      Restart = "always";
-    };
   };
 }
