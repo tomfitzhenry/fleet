d8712d3d8b7fd49d3bc20bc378993b6c1070f4cf
Tom Fitzhenry <tom@tom-fitzhenry.me.uk>
2026-04-18 08:02:14 +1000

    tlshd: use the tlshd modular service

--- modules/tlshd/default.nix
+++ modules/tlshd/default.nix
@@ -7,8 +7,6 @@
 }:
 let
   cfg = config.tomf.tlshd;
-  iniFormat = pkgs.formats.ini { };
-  configFile = iniFormat.generate "tlshd.conf" cfg.settings;
 in
 {
   options = {
@@ -17,67 +15,23 @@ in
         type = lib.types.bool;
         default = false;
       };
-      package = lib.mkOption {
-        type = lib.types.package;
-        default = pkgs.ktls-utils;
-        description = "The ktls-utils package providing tlshd.";
-      };
       settings = lib.mkOption {
-        type = iniFormat.type;
+        type = lib.types.attrsOf (lib.types.attrsOf lib.types.str);
         default = { };
+        description = ''
+          Configuration for tlshd in INI format.
+          See {manpage}`tlshd.conf(5)` for available options.
+        '';
       };
     };
   };
   config = lib.mkIf cfg.enable {
     boot.kernelModules = [ "tls" ];
 
-    systemd.services.tlshd = {
-      description = "TLS Handshake Daemon";
-      after = [ "network.target" ];
-      wantedBy = [ "multi-user.target" ];
-
-      serviceConfig = {
-        Type = "simple";
-        ExecStart = "${cfg.package}/bin/tlshd --stderr --config ${configFile}";
-        Restart = "on-failure";
-        StateDirectory = "tlshd";
-
-        # The privileges that the daemon needs.
-        User = "root";
-        Group = "root";
-        PrivateUsers = false;
-        PrivateNetwork = false;
-        RestrictAddressFamilies = [ "AF_NETLINK" ];
-        CapabilityBoundingSet = [ "CAP_NET_ADMIN" ];
-        AmbientCapabilities = [ "CAP_NET_ADMIN" ];
-
-        # Restrict other privileges.
-        MemoryDenyWriteExecute = true;
-        NoNewPrivileges = true;
-        PrivateDevices = true;
-        PrivateTmp = true;
-        ProcSubset = "pid";
-        ProtectClock = true;
-        ProtectSystem = "strict";
-        ProtectHome = true;
-        ProtectControlGroups = true;
-        ProtectKernelModules = true;
-        ProtectKernelTunables = true;
-        ProtectKernelLogs = true;
-        ProtectHostname = true;
-        ProtectProc = "invisible";
-        RestrictNamespaces = true;
-        RestrictSUIDSGID = true;
-        RestrictRealtime = true;
-        LockPersonality = true;
-        SystemCallFilter = [
-          "@system-service"
-          "~@privileged"
-          "~@resources"
-        ];
-        SystemCallArchitectures = "native";
-        UMask = "0077";
-      };
+    system.services.tlshd = {
+      imports = [ (lib.modules.importApply ../../pkgs/ktls-utils/service.nix { }) ];
+      tlshd.package = pkgs.ktls-utils;
+      tlshd.settings = cfg.settings;
     };
   };
 }
