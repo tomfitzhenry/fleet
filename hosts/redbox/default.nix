53ec86c8cb53f0edb83a7e961bba6284042a2321
Tom Fitzhenry <tom@tom-fitzhenry.me.uk>
2026-07-04 20:08:54 +1000

    redbox: migrate to BIOS to UEFI
    I flashed my APU1 with Coreboot 4.18 with EDK2 payload, so now it can boot UEFI.

--- hosts/redbox/default.nix
+++ hosts/redbox/default.nix
@@ -21,15 +21,9 @@ in
   nixpkgs.system = "x86_64-linux";
   system.stateVersion = "25.05";
 
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/sda";
+  boot.loader.systemd-boot.enable = true;
 
   boot.kernelParams = [ "console=ttyS0,115200n8" ];
-  boot.loader.grub.extraConfig = "
-    serial --speed=115200 --unit=0 --word=8 --parity=no --stop=1
-    terminal_input serial
-    terminal_output serial
-  ";
 
   tomf = {
     podman.enable = true;
