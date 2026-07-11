db3b460a31492ae3c9212ccdba080aba060c1c1c
Tom Fitzhenry <tom@tom-fitzhenry.me.uk>
2026-07-12 06:36:29 +1000

    feed: serve readeck

--- hosts/feed/default.nix
+++ hosts/feed/default.nix
@@ -20,14 +20,32 @@
   services.caddy = {
     enable = true;
     configFile = pkgs.writeText "Caddyfile" ''
-      import /etc/caddy/vhost { } {
+      import /etc/caddy/vhost.readeck { } {
         basic_auth {
           tom $2a$14$w.rkYXmgon7phJLm.6689OT9w0iGqkXbM6f9huI7YBJUDlHq5tY5y
         }
-        reverse_proxy 127.0.0.1:${toString config.services.yarr.port}
+        reverse_proxy 127.0.0.1:${toString config.services.readeck.settings.server.port} {
+          header_up -Authorization
+        }
+      }
+      import /etc/caddy/vhost.yarr { } {
+        basic_auth {
+          tom $2a$14$w.rkYXmgon7phJLm.6689OT9w0iGqkXbM6f9huI7YBJUDlHq5tY5y
+        }
+        reverse_proxy 127.0.0.1:${toString config.services.yarr.port} {
+          header_up -Authorization
+        }
       }
     '';
   };
 
+  services.readeck = {
+    enable = true;
+    # Needs READECK_SECRET_KEY=
+    environmentFile = "/var/lib/readeck/env";
+    settings = {
+      server.port = 8000;
+    };
+  };
   services.yarr.enable = true;
 }
