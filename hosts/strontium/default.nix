4c1c4bb6a46af0af078be9ed31c8a78d1ce5b325
Tom Fitzhenry <tom@tom-fitzhenry.me.uk>
2026-07-22 20:54:19 +1000

    strontium: update knot dns key

--- hosts/strontium/default.nix
+++ hosts/strontium/default.nix
@@ -164,7 +164,7 @@
             # private key held non-exportably in the device:
             # $ yubico-piv-tool -s 82 -a read-certificate -o slot82-cert.pem
             # $ openssl x509 -in slot82-cert.pem -pubkey -noout | openssl pkey -pubin -outform DER | openssl dgst -sha256 -binary | openssl enc -base64
-            "UC6xiEWQdQB9cCPr0l/v/FeP7eU4bwjClizNdbMFxyU="
+            "FBV8rIeMwuopp+mNvoyjrUXOqO7pHgWfpKhyy/w87x0="
           ];
           action = [
             # $ kdig @127.0.0.1 -p 8853 -t AXFR $DOMAIN +tls-certfile=ecdsa_cert.pem +tls-keyfile=ecdsa_key.pem +tls-pin=60VI3zVqodGsKHT7q1c3KcWkmbAxh7VgR4+0YFhY6qo=
